a1783e64d37c01c1962e2f18f945ad30e8fc7d27
TimTan <beasttran018@gmail.com>
2022-08-16 14:21:16 +0800

    v 4 - fixed a link issue for a project

--- components/Content.js
+++ components/Content.js
@@ -57,7 +57,7 @@ export default function Content(props) {
                     </div>
                     <div>
                         <div className='animate__animated animate__slideInRight'>
-                            <a href="google-clone-tanvirbhachu.vercel.app" target="_blank" rel="noreferrer"
+                            <a href="https://google-clone-tanvirbhachu.vercel.app/" target="_blank" rel="noreferrer"
                                 className="block bg-dark/80 w-full p-5 hover:bg-[#212121] rounded-xl transition hover:scale-105 border-neutral-700 border-2 hover:border-emerald-700">
                                 <h3 className="text-2xl mb-2 text-white">Google Search Clone</h3>
                                 <p className="text-neutral-400 text-sm">Not a complete 1:1 clone as I added my own twist to it. I built this using Next JS and Tailwind CSS. 
